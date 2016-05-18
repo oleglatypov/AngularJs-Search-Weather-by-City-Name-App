a4a8ce3a4564ad0177d4a2844b1fcb3147edc820
oleglatypov <ovlatypov@gmail.com>
2016-05-18 19:36:41 -0400

    Add On Enter key submit

--- js/controllers/controllers.js
+++ js/controllers/controllers.js
@@ -3,11 +3,16 @@
  */
 
 // CONTROLLERS
-weatherApp.controller('mainController',['$scope', 'cityService', function ($scope, cityService){
+weatherApp.controller('mainController',['$scope','$location', 'cityService', function ($scope,$location, cityService){
     $scope.city = cityService.city;
+
     $scope.$watch('city', function () {
         cityService.city = $scope.city;
-    })
+    });
+
+    $scope.submit = function () {
+        $location.path("/forecast");
+    };
 }]);
 
 weatherApp.controller('foreCastController',['$scope','$resource','$routeParams', 'cityService', function ($scope,$resource,$routeParams, cityService){
